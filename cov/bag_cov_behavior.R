b0f25f3c0bad0e2f75c60bcf36d116721c34c622
jinbora0720 <jinbora0720@gmail.com>
2022-08-29 20:37:00 +0200

    add proposition 3

--- cov/bag_cov_behavior.R
+++ cov/bag_cov_behavior.R
@@ -1,5 +1,5 @@
 # Nonstationarity of G-BAGs #
-# 1 Figure 
+# 2 Figures
 rm(list = ls())
 
 # dependencies
@@ -13,6 +13,63 @@ path <- "~/GBAGs/"
 
 # plot extensions
 extension <- c(".pdf", ".eps", ".png")
+
+###############
+# Proposition #
+###############
+# spatiotemporal w/ Apanasovich and Genton (2010) 
+N <- 100
+a <- 0.07
+c <- 0.01
+kappa <- 1
+sig_sq <- 1
+h <- seq(0, 12, length = N)
+u <- seq(0, 12, length = N)
+hu <- expand.grid(h, u)
+
+basedata <- data.frame(h = hu[,1], u = hu[,2]) %>% 
+  mutate(invaup1 = 1/((a*u^2+1)^kappa)) %>% 
+  mutate(z = sig_sq*invaup1/((a*h^2+1)^(kappa/2))*exp(-c*h^2*invaup1-c*u^2)) %>% 
+  select(-invaup1)
+
+p <- c(0.51, 0.55, seq(0.6, 0.9, by = 0.1), 0.95, 0.99)
+Ctilde12 = Ctilde13 <- matrix(0, nrow = N^2, ncol = length(p))
+for (i in 1:length(p)) {
+  Ctilde12[,i] <- p[i]*basedata$z
+  Ctilde13[,i] <- (1-p[i])*basedata$z
+}
+
+g0 <- data.frame(h = rep(basedata$h, times = length(p)), 
+           u = rep(basedata$u, times = length(p)), 
+           p = rep(paste0("p = ", p), each = N^2), 
+           nonstCov12 = as.numeric(Ctilde12), 
+           nonstCov13 = as.numeric(Ctilde13)) %>% 
+  pivot_longer(c("nonstCov12", "nonstCov13"), 
+               names_to = "what", values_to = "Cov") %>% 
+  filter(p %in% paste0("p = ", c(0.51, 0.55, 0.7))) %>% 
+  ggplot() + 
+  facet_wrap(~p, ncol = 3) + 
+  geom_contour(aes(x = h, y = u, z = Cov, color = what, linetype = what), 
+               breaks = 0.1) +
+  scale_color_manual(values = c("nonstCov12" = "#E69F00", 
+                                "nonstCov13" = "#009E73"), 
+                     labels = c(expression(paste(s[1],"&",s[2])), 
+                                expression(paste(s[1],"&",s[3])))) +
+  scale_linetype_manual(values = c("nonstCov12" = 1, 
+                                   "nonstCov13" = 2), 
+                        labels = c(expression(paste(s[1],"&",s[2])), 
+                                   expression(paste(s[1],"&",s[3])))) + 
+  labs(x = "||h||", y = "|u|", 
+       title = "G-BAG induced covariance: nonstationarity", 
+       color = "", linetype = "") +
+  theme(legend.position = "bottom", 
+        plot.margin = margin(t = 1, l = 1, r = 8, b = -2),
+        legend.margin = margin(b = 0, r = 0, t = -10, l = 0), 
+        legend.text = element_text(size = 11))
+# for (ext in extension) {
+#   ggsave(plot = g0, paste0(path, "plots/cov_proposition", ext),
+#          width = 7, height = 2.5)
+# }
 
 ########
 # Data #
